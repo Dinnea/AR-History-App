1f091a4bb691ae5a870cb6eb01d8874f653b2a73
Lisanne <500856@student.saxion.nl>
2021-11-22 16:23:41 +0100

    Zoom WIP (not working yet)
    I copied some code from the internet but it needs to be adjusted.

--- Assets/Scripts/TapThePOI.cs
+++ Assets/Scripts/TapThePOI.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mapbox.Unity.Map;
 
 public class TapThePOI : MonoBehaviour
 {
     void OnMouseDown()
     {
         Debug.Log(name);
+
+        //TO DO: display info pages
     }
 }
