2fab8e879d5592f81cc91407e728ded82a6dcd63
Lisanne <500856@student.saxion.nl>
2021-11-23 11:09:26 +0100

    Zoom
    Zooming might work, no labels yet.

--- Assets/Scripts/PanZoom.cs
+++ Assets/Scripts/PanZoom.cs
@@ -7,16 +7,14 @@ public class PanZoom : MonoBehaviour
     Vector3 touchStart;
     public float zoomOutMin = 1;
     public float zoomOutMax = 8;
+    public float zoomSpeed = 1;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            touchStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        }
         if (Input.touchCount == 2)
         {
+            Debug.Log("touchCount 2");
             Touch touchZero = Input.GetTouch(0);
             Touch touchOne = Input.GetTouch(1);
 
@@ -28,18 +26,19 @@ public class PanZoom : MonoBehaviour
 
             float difference = currentMagnitude - prevMagnitude;
 
-            zoom(difference * 0.01f);
+            zoom(difference * zoomSpeed);
         }
-        else if (Input.GetMouseButton(0))
-        {
-            Vector3 direction = touchStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Camera.main.transform.position += direction;
-        }
-        zoom(Input.GetAxis("Mouse ScrollWheel"));
     }
 
     void zoom(float increment)
     {
-        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - increment, zoomOutMin, zoomOutMax);
+        if (Camera.main.transform.position.y + increment > zoomOutMin && Camera.main.transform.position.y + increment < zoomOutMax)
+        {
+            if (Input.touchCount == 2)
+            {
+                Camera.main.transform.position += new Vector3(0, increment, 0);
+            }
+
+        }
     }
 }
