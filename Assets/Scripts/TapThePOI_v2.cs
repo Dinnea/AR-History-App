9f0111181955a1aa9b6f40d4bfd11934ae7e684e
Izabella Wojtowicz <izabella.wojtowicz@gmail.com>
2021-11-25 14:04:09 +0100

    Working!

--- Assets/Scripts/TapThePOI_v2.cs
+++ Assets/Scripts/TapThePOI_v2.cs
@@ -7,18 +7,31 @@ public class TapThePOI_v2 : MonoBehaviour
     private bool _hit;
     private RaycastHit _hitInfo = new RaycastHit();
     private GameObject _tapped;
+    [SerializeField] GameObject _ui;
     private void Update()
     {
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) 
         {
             _hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.GetTouch(0).position), out _hitInfo);
-            _tapped = _hitInfo.transform.gameObject;
-            if () 
+            if(_hit) 
+
             {
-                if (_hitInfo.transform.gameObject.name == "Twente Airport")
+                _tapped = _hitInfo.transform.gameObject;
+
+                Vector2 userPosition = new Vector2(transform.position.x, transform.position.z);
+                Vector2 tappedPosiiton = new Vector2(_tapped.transform.position.x, _tapped.transform.position.z);
+                float distance = tappedPosiiton.magnitude - userPosition.magnitude;
+
+                Debug.Log(distance);
+                if (Mathf.Abs(distance) < 10)
                 {
+                    if (_hitInfo.transform.gameObject.name == "Twente Airport")
+                    {
+                        _ui.SetActive(true);
+                    }
                 }
             }
+            
             
         }
     }
