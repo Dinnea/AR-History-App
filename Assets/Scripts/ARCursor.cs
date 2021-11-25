3cefbb5148898f97c97f8d7739ccce1562dce7bc
Izabella Wojtowicz <izabella.wojtowicz@gmail.com>
2021-11-25 09:59:01 +0100

    Better UI display?

--- Assets/Scripts/ARCursor.cs
+++ Assets/Scripts/ARCursor.cs
@@ -20,7 +20,7 @@ public class ARCursor : MonoBehaviour
     private void Start()
     {
         cursor.SetActive(useCursor);
-        StartCoroutine(Box2());
+        
     }
     private void Update()
     {
@@ -67,32 +67,7 @@ public class ARCursor : MonoBehaviour
         }
     }
 
-    IEnumerator Box2(float time = 7)
-    {
-        yield return new WaitForSeconds(time);
-
-        popUps[0].SetActive(false);
-        popUps[1].SetActive(true);
-
-        StartCoroutine(Box3());
-    }
-
-    IEnumerator Box3(float time = 5) 
-    {
-        yield return new WaitForSeconds(time);
-
-        popUps[1].SetActive(false);
-        popUps[2].SetActive(true);
-
-        StartCoroutine(Box4());
-    }
-
-    IEnumerator Box4(float time = 5) 
-    {
-        yield return new WaitForSeconds(time);
-
-        popUps[2].SetActive(false);
-    }
+   
         // Code to execute after the delay
         //iteration 1:
         //turn off panel #1
--- Assets/Scripts/AR_UI.cs
+++ Assets/Scripts/AR_UI.cs
@@ -1,14 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
 public class AR_UI : MonoBehaviour
 {
     [SerializeField] List<GameObject> popUps = new List<GameObject>(4);
+    [SerializeField] ARPlaneManager _aRPlaneManager;
+    bool _wasDisplayed = false;
 
     private void Start()
     {
-        StartCoroutine(Box2());
+        //StartCoroutine(Box2());
+    }
+
+    private void Update()
+    {
+        if (_aRPlaneManager.trackables.count > 0 && !_wasDisplayed) 
+        {
+            popUps[0].SetActive(false);
+            popUps[1].SetActive(true);
+            _wasDisplayed = true;
+            StartCoroutine(Box3());
+        }
+
     }
     IEnumerator Box2(float time = 7)
     {
