f11db4fe32c943f92375d33247c4c7757e4c17c7
Izabella Wojtowicz <izabella.wojtowicz@gmail.com>
2021-11-25 09:49:32 +0100

    moved the UI script

--- Assets/Scripts/AR_UI.cs
+++ Assets/Scripts/AR_UI.cs
@@ -5,63 +5,35 @@ using UnityEngine;
 public class AR_UI : MonoBehaviour
 {
     [SerializeField] List<GameObject> popUps = new List<GameObject>(4);
-    private int _number = 0;
-    private int _oldNumber = 0;
 
     private void Start()
     {
-        _oldNumber = _number;
+        StartCoroutine(Box2());
+    }
+    IEnumerator Box2(float time = 7)
+    {
+        yield return new WaitForSeconds(time);
+
+        popUps[0].SetActive(false);
+        popUps[1].SetActive(true);
+
+        StartCoroutine(Box3());
     }
 
-    private void Update()
+    IEnumerator Box3(float time = 5)
     {
-        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
-        {
-            if (_number<3 && _number != -1) _number++;
-            Debug.Log(_number);
-            Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
-            RaycastHit raycastHit;
-            if (Physics.Raycast(raycast, out raycastHit))
-            {
-                if (raycastHit.collider.CompareTag("Plane"))
-                {
-                    popUps[3].SetActive(true);
-                }
-            }
-           ;
+        yield return new WaitForSeconds(time);
 
-        }
+        popUps[1].SetActive(false);
+        popUps[2].SetActive(true);
 
-        if (_oldNumber != _number) 
-        {
-            _oldNumber = _number;
-            switch (_number) 
-            {
-                case -1:
-                    for (int i = 0; i < 4; i++)
-                    {
-                        popUps[i].SetActive(false);
-                    }
-                    break;
-                case 1:
-                    popUps[0].SetActive(false);
-                    popUps[1].SetActive(true);
-                    break;
-                case 2:
-                    popUps[1].SetActive(false);
-                    popUps[2].SetActive(true);
-                    break;
-                case 3:
-                    popUps[2].SetActive(false);
-                    break;
-                case 4:
-                    for (int i = 0; i<3; i++) 
-                    {
-                        popUps[i].SetActive(false);
-                    }
-                    
-                    break;
-            }
-        }
+        StartCoroutine(Box4());
+    }
+
+    IEnumerator Box4(float time = 5)
+    {
+        yield return new WaitForSeconds(time);
+
+        popUps[2].SetActive(false);
     }
 }
