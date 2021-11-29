3f5e5a0e574daa8155cc3cf2056fc0dc28a36691
Lisanne <500856@student.saxion.nl>
2021-11-29 15:02:08 +0100

    script name changed back

--- Assets/Scripts/TiltLabel.cs
+++ Assets/Scripts/TiltLabel.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LookAtCam : MonoBehaviour
+public class TiltLabel : MonoBehaviour
 {
     private void Update()
     {
