764d4ce58f123c66015e1e5ffbbd728e8d8642ec
Lisanne <500856@student.saxion.nl>
2021-11-29 12:40:52 +0100

    2D pin, 3D pin (no texture)
    - Added character 3D pin (no texture yet)
    - Changed script name TiltLabel to LookAtCam
    - Added character 2D pin

--- Assets/Scripts/LookAtCam.cs
+++ Assets/Scripts/LookAtCam.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TiltLabel : MonoBehaviour
+public class LookAtCam : MonoBehaviour
 {
     private void Update()
     {
